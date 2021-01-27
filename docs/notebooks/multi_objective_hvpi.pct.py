c3fa035ebab9fcaaf4dfb87ed96e7368b1e5c101
FlyWill <tsingjixiang@yahoo.com>
2021-01-27 13:14:12 +0100

    update notebook: add 3-obj toy function

--- docs/notebooks/multi_objective_hvpi.pct.py
+++ docs/notebooks/multi_objective_hvpi.pct.py
@@ -72,9 +72,9 @@ plt.show()
 #
 # We'll model the different objective functions with their own Gaussian process regression models.
 
-def create_bo_model(data):
+def create_bo_model(data, input_dim=2):
     variance = tf.math.reduce_variance(data.observations)
-    lengthscale = 1.0 * np.ones(2, dtype=gpflow.default_float())
+    lengthscale = 1.0 * np.ones(input_dim, dtype=gpflow.default_float())
     kernel = gpflow.kernels.Matern52(variance=variance, lengthscales=lengthscale)
     gpr = gpflow.models.GPR(data.astuple(), kernel, noise_variance=1e-5)
     gpflow.set_trainable(gpr.likelihood, False)
@@ -118,6 +118,74 @@ plt.show()
 # -
 
 # ... and visulize in the objective space, orange dots denotes the nondominated points.
+
+plot_bo_points_in_obj_space(result.try_get_final_datasets(), num_init=num_initial_points)
+plt.show()
+
+# ## Advanced: Problem with 3 Objective Function
+
+# Now we demonstrate an optimization for DTLZ2 function with 3 objectives in 4 dimension:
+
+# +
+from tensorflow import zeros, sin, cos
+from math import pi
+
+def dtlz2(x: TensorType, M: int = 3) -> TensorType:
+    """
+    DTLZ test problem 2.
+    """
+
+    def g(xm):
+        z = xm - 0.5
+        return tf.reduce_sum(z ** 2, axis=1, keepdims=True)
+
+    def problem2(x, M):
+        f = None
+
+        for i in range(M):
+            y = (1 + g(x[:, M-1:]))
+            for j in range(M - 1 - i):
+                y *= cos((pi * x[:, j, np.newaxis]) / 2)
+            if i > 0:
+                y *= sin((pi * x[:, M - 1 - i, np.newaxis]) / 2)
+            f = y if f is None else tf.concat([f, y], 1)
+        return f
+
+    return problem2(x, M)
+
+def observer(query_points):
+    y = dtlz2(query_points)
+    return {OBJECTIVES[i]: trieste.data.Dataset(query_points, y[:, i, np.newaxis]) for i in range(num_objective)}
+
+
+# -
+
+# Now we can follow similar setup to optimize this problem, it may take a while waiting for the finish of the optimizer
+
+# +
+mins = [0, 0, 0, 0]
+maxs = [1, 1, 1, 1]
+lower_bound = tf.cast(mins, gpflow.default_float())
+upper_bound = tf.cast(maxs, gpflow.default_float())
+search_space = trieste.space.Box(lower_bound, upper_bound)
+
+num_objective = 3
+OBJECTIVES = ["OBJECTIVE{}".format(i + 1) for i in range(num_objective)]
+
+
+num_initial_points = 10
+initial_query_points = search_space.sample(num_initial_points)
+initial_data = observer(initial_query_points)
+
+models = {OBJECTIVES[i]: create_bo_model(initial_data[OBJECTIVES[i]], 4) for i in range(num_objective)}
+
+hvpi = HVProbabilityOfImprovement()
+rule = trieste.acquisition.rule.EfficientGlobalOptimization(builder=hvpi)
+
+num_steps = 50
+bo = trieste.bayesian_optimizer.BayesianOptimizer(observer, search_space)
+result = bo.optimize(num_steps, initial_data, models, acquisition_rule=rule)
+# -
 
 plot_bo_points_in_obj_space(result.try_get_final_datasets(), num_init=num_initial_points)
 plt.show()

eb4a0ea949a3baec7e7ccc3785301c0f5e5a05cf
FlyWill <tsingjixiang@yahoo.com>
2021-05-18 19:12:18 +0200

    finished rff

--- docs/notebooks/pfes.pct.py
+++ docs/notebooks/pfes.pct.py
@@ -146,7 +146,9 @@
 
 # -----------------
 
-# ## Sample GP Posterior in Weight space approximation by RFF
+# # Sample GP Posterior in Weight space approximation by RFF
+
+# ## 1D case
 
 # +
 import numpy as np
@@ -170,12 +172,10 @@ tf.keras.backend.set_floatx("float64")
 data = np.genfromtxt("util/data/regression_1D.csv", delimiter=",")
 X = data[:, 0].reshape(-1, 1)
 Y = data[:, 1].reshape(-1, 1)
-k = gpflow.kernels.Matern52()
+k = gpflow.kernels.RBF()
 m = gpflow.models.GPR(data=(X, Y), kernel=k, mean_function=None)
 opt = gpflow.optimizers.Scipy()
 opt_logs = opt.minimize(m.training_loss, m.trainable_variables, options=dict(maxiter=100))
-
-# ### GPFlowExtra ver
 
 # Here we manually do the RFF by Mahimi
 
@@ -192,8 +192,6 @@ opt_logs = opt.minimize(m.training_loss, m.trainable_variables, options=dict(max
 # Below we show how the RFF can be conducted *manually*
 
 # --------------
-
-# ## GPFlowExtra Version
 
 # $$
 # k_{unit}(X, X^\prime) \approx \frac{1}{D}\sum_{i=1}^D z_{{\omega}_i}(X) z_{{\omega}_i}(X^\prime),
@@ -317,375 +315,86 @@ _ = plt.xlim(-0.1, 1.1)
 
 # --------------
 
-# ## Updated Version (Compatible to TF)
-
-# +
-m_ftrs = 1000
-input_dim = X.shape[1]
-
-l = np.array([np.atleast_1d(m.kernel.lengthscales.numpy())] * m_ftrs) # [m_ftrs, dim]
-# 这一步真的看不懂, W 应该是一个 m_ftrs * input_dim的
-W = np.divide(np.random.randn(m_ftrs, input_dim), l) # [m_ftrs, dim]
-
-# b ~ U[0, 2pi]
-b = 2 * np.pi * np.random.uniform(0, 1, m_ftrs) # a.k.a, θ for spectral density, normal is the density of RBF kernel
-# Fixedme: should be dynamic
-# b_for_nObservations = np.array([b, ] * Nxn).T # Nx * b
-b_for_nObservations = np.array([b, ] * X.shape[0]).T  # Nx * b
-
-kernel_var = m.kernel.variance.numpy()
-
-# \Phi with dimensions mxn: φ(x) = \sqrt(2a/m) cos(Wx + b)
-# FIXedME: change to scaled version
-# vector_phi_T = np.sqrt(2 * alpha / m_ftrs) * \
-#                      np.cos(np.dot(W, models[obj_id].X.value.T) + b_for_nObservations)
-vector_phi_T = np.sqrt(2 * kernel_var / m_ftrs) * \
-               np.cos(np.dot(W, X.T) + b_for_nObservations)
-
-
-
-# -
-
-# $$
-# p(\textbf{w} | \mathcal{D}) = \frac{\prod_{n=1}^N p(y_n| \textbf{w}^\intercal \boldsymbol{\phi}(X_n), \sigma_\epsilon^2) p(\textbf{w})}{p(\mathcal{D})},
-# $$
-# where we assume $p(\textbf{w})$ to be a standard normal multivariate prior and $p(y_n| \textbf{w}^\intercal \boldsymbol{\phi}(X_n), \sigma_\epsilon^2)$ to be a univariate Gaussian observation model of the i.i.d. likelihood with mean $\textbf{w}^\intercal \boldsymbol{\phi}(X_n)$ and noise variance $\sigma_\epsilon^2$. The boldface notation $\boldsymbol{\phi}(X_n)$ refers to the vector-valued feature function that evaluates all features from $1$ up to $I$ for one particular input $X_n$. Under these assumptions, the posterior $p(\textbf{w} | \mathcal{D})$ enjoys a closed form and is Gaussian. Predictions can readily be obtained by sampling $\textbf{w}$ and evaluating the function sample $\textbf{w}$ at new locations $\{X_{n^\star}^\star\}_{n^\star=1,...,N^\star}$ as $\{\textbf{w}^\intercal \boldsymbol{\phi}(X_{n^\star}^\star)\}_{n^\star=1,...,N^\star}$.
-
-# The posterior distribution of $p(\textbf{w})$ has been given as:
-
-# Hence we could sample from $p(\textbf{w})$ in order to sample the weighted spce approximated Gaussian Process posterior
-
-# +
-# A: φ(x)^T φ(x)/σ^2  + I
-A = np.divide(np.dot(vector_phi_T, vector_phi_T.T), m.likelihood.variance.numpy()) + np.eye(m_ftrs)
-
-# posterior of θ sample: θ|Dn ∼ N(A−1ΦTyn, σ2A−1), shape: (m_ftrs), shape checked correct
-A_inverse = tf.linalg.inv(A).numpy()
-
-# θ(A^{-1} φ(x)^T Y / σ^2, )
-mean_of_post_theta = np.divide(np.dot(np.dot(A_inverse, vector_phi_T), Y), m.likelihood.variance.numpy())
-mean_of_post_theta = np.squeeze(np.asarray(mean_of_post_theta))
-variance_of_post_theta = A_inverse
-
-theta_sample = np.random.multivariate_normal(mean_of_post_theta, variance_of_post_theta)
-
-
-def makeFunc(alpha, m_ftrs, W, b, theta):
-    """
-    :param alpha
-    :param m_ftrs
-    :param W
-    :param b
-    :param theta
-    """
-    return lambda x: np.dot(np.sqrt(2 * alpha / m_ftrs) * np.cos(np.dot(W, np.atleast_2d(x).T) + np.atleast_2d(b).T).T,
-                            theta)
-
-
-# +
-## generate test points for prediction
-xx = np.linspace(-0.1, 1.1, 100).reshape(100, 1)  # test points must be of shape (N, D)
-
-## predict mean and variance of latent GP at test points
-mean, var = m.predict_f(xx)
-
-## generate 10 samples from posterior
-tf.random.set_seed(1)  # for reproducibility
-samples = m.predict_f_samples(xx, 10)  # shape (10, 100, 1)
-
-    
-## plot
-plt.figure(figsize=(12, 6))
-plt.plot(X, Y, "kx", mew=2)
-plt.plot(xx, mean, "C0", lw=2)
-plt.fill_between(
-    xx[:, 0],
-    mean[:, 0] - 1.96 * np.sqrt(var[:, 0]),
-    mean[:, 0] + 1.96 * np.sqrt(var[:, 0]),
-    color="C0",
-    alpha=0.2,
-)
-
-for _ in range(100):
-    theta_sample = np.random.multivariate_normal(mean_of_post_theta, variance_of_post_theta)
-    f_sample = makeFunc(kernel_var, m_ftrs, W, b, theta_sample)
-    yy = f_sample(xx)
-    plt.plot(xx, yy, color="C1")
-
-
-plt.plot(xx, samples[:, :, 0].numpy().T, "C0", linewidth=0.5)
-_ = plt.xlim(-0.1, 1.1)
-
-# -
-
-# ### Gpflux
-
-# Test whether this two $\phi$ has similarity
-
-# ---------------------------
-
-# The sampled $\phi_i$ function in RFF can be obtained by `gpflux`'s `RandomFourierFeatures` functionality directly
-
-# +
-from typing import Callable
-
-num_rff = 1000
-features = RandomFourierFeatures(m.kernel, num_rff, dtype=X.dtype)
-# -
-
-# Get the distribution of weight: $\theta$
-
-# +
-from tensorflow_probability import distributions as tfd 
-
-Phi = features(tf.cast(X, dtype=tf.float64))
-# A_inv = tf.linalg.inv(tf.linalg.matmul(Phi, Phi, transpose_a=True) + 
-#                   tf.cast(m.likelihood.variance, dtype=tf.float64) * 
-#                   tf.eye(tf.shape(Phi)[1], dtype=tf.float64))
-
-# 这里是使用了GPFlowextra的版本: A_inv = [φ(x)^T φ(x)/σ^2 +  I]
-A_inv = tf.linalg.inv(
-    tf.linalg.matmul(vector_phi_T.T, vector_phi_T.T, transpose_a=True)/
-    tf.cast(m.likelihood.variance, dtype=tf.float64)  + 
-    tf.eye(tf.shape(vector_phi_T.T)[1], dtype=tf.float64))
-post_theta_mean = tf.matmul(tf.matmul(A_inv, vector_phi_T.T, transpose_b = True), Y)  / tf.cast(m.likelihood.variance, dtype=tf.float64)
-post_theta_cov = A_inv
-
-
-post_theta_sampler = tfd.MultivariateNormalFullCovariance(post_theta_mean, 
-                                                          post_theta_cov)
-
-def makeFunc(feature_func, theta):
-    return lambda x: tf.matmul(feature_func(x), theta)
-
-
-# +
-## generate test points for prediction
-xx = np.linspace(-0.1, 1.1, 100).reshape(100, 1)  # test points must be of shape (N, D)
-
-## predict mean and variance of latent GP at test points
-mean, var = m.predict_f(xx)
-
-## generate 10 samples from posterior
-tf.random.set_seed(1)  # for reproducibility
-samples = m.predict_f_samples(xx, 10)  # shape (10, 100, 1)
-
-    
-## plot
-plt.figure(figsize=(12, 6))
-plt.plot(X, Y, "kx", mew=2)
-plt.plot(xx, mean, "C0", lw=2)
-plt.fill_between(
-    xx[:, 0],
-    mean[:, 0] - 1.96 * np.sqrt(var[:, 0]),
-    mean[:, 0] + 1.96 * np.sqrt(var[:, 0]),
-    color="C0",
-    alpha=0.2,
-)
-
-for _ in range(10):
-    f_sample = makeFunc(features,  
-                        np.random.multivariate_normal(mean_of_post_theta, 
-                                                      variance_of_post_theta)[..., np.newaxis])
-    yy = f_sample(xx)
-    plt.plot(xx, yy, color="C1")
-
-plt.plot(xx, samples[:, :, 0].numpy().T, "C0", linewidth=0.5)
-_ = plt.xlim(-0.1, 1.1)
-
-# -
-
-# ### Approach 2
-
-# Approaximate the kernel with RFF
-
-# +
-from typing import Callable
-
-kernel = gpflow.kernels.Matern52()
-num_rff = 10000
-features = RandomFourierFeatures(kernel, num_rff, dtype=X.dtype)
-coefficients = tf.ones((num_rff, 1), dtype=X.dtype)
-kernel_with_features = KernelWithFeatureDecomposition(kernel, features, coefficients)
-
-
-# -
-
-# $$\textbf{f}^\star \approx \boldsymbol{\Phi}^\star \textbf{w} + \boldsymbol{\Phi}^\star \boldsymbol{\Phi}^\intercal(\boldsymbol{\Phi} \boldsymbol{\Phi}^\intercal + \sigma_\epsilon^2 \textbf{I})^{-1}(\textbf{y} - \boldsymbol{\Phi} \textbf{w}) \; \text{ where } \; \textbf{w} \sim \mathcal{N}(\textbf{0}, \textbf{I}),$$
-
-#
-# with $\boldsymbol{\Phi}$ referring to the $N \times D$ feature matrix evaluated at the training points $\{X_n\}_{n=1,...,N}$ and $\boldsymbol{\Phi}^\star$ to the $N^\star \times D$ feature matrix evaluated at the test points $\{X^\star_{n^\star}\}_{n^\star=1,...,N^\star}$. The quantities $\boldsymbol{\Phi}^\star \boldsymbol{\Phi}^\intercal$ and $\boldsymbol{\Phi} \boldsymbol{\Phi}^\intercal$ are weight space approximations of the exact kernel matrices $\textbf{K}_{\textbf{f}^\star \textbf{f}}$ and $\textbf{K}_{\textbf{f} \textbf{f}}$ respectively. The weight space Matheron representation enables you to sample more efficiently with a complexity of $\mathcal{O}(D)$ that scales only linearly with the number of feature functions $D$ (because the standard normal weight prior's diagonal covariance matrix can be linearly Cholesky decomposed). The problem is that many feature functions are required in order to approximate the exact posterior reasonably well in areas most relevant for extrapolation (i.e. close but not within the training data), as shown by Wilson et al. and as reproduced in another `gpflux` notebook.
-
-def rff_wsa_fsample(x, w):
-    Phi_star = features(x)
-    Phi = features(X)
-    a = tf.linalg.matmul(Phi_star, Phi, transpose_b=True)
-    b = tf.linalg.inv(tf.linalg.matmul(Phi, Phi, transpose_b=True))
-    c = Y - tf.matmul(Phi, w)
-    return tf.linalg.matmul(Phi_star, w) + tf.matmul(tf.matmul(a, b), c) 
-
-
-for _ in range(20):
-    w = tfp.distributions.Normal([[0]] * num_rff, [[1]] * num_rff).sample()
-    xx = np.linspace(-0.1, 1.0, 100).reshape(100, 1)  # test points must be of shape (N, D)
-    yy = rff_wsa_fsample(xx, tf.cast(w, dtype=tf.float64))
-    plt.plot(xx, yy, color="C1")
-
-# +
-# 这个inducing point  是啥啊
-Z = tf.gather(data, [0], axis=1)
-inducing_variable = gpflow.inducing_variables.InducingPoints(Z)
-gpflow.utilities.set_trainable(inducing_variable, False)
-
-layer = gpflux.layers.GPLayer(
-    kernel_with_features,
-    inducing_variable,
-    data.shape[0],
-    whiten=False,
-    num_latent_gps=1,
-    mean_function=gpflow.mean_functions.Zero(),
-)
-
-likelihood_layer = gpflux.layers.LikelihoodLayer(gpflow.likelihoods.Gaussian())  # noqa: E231
-dgp = gpflux.models.DeepGP([layer], likelihood_layer)
-
-# +
-## generate test points for prediction
-xx = np.linspace(-0.1, 1.1, 100).reshape(100, 1)  # test points must be of shape (N, D)
-
-## predict mean and variance of latent GP at test points
-mean, var = m.predict_f(xx)
-
-## generate 10 samples from posterior
-tf.random.set_seed(1)  # for reproducibility
-samples = m.predict_f_samples(xx, 10)  # shape (10, 100, 1)
-
-    
-## plot
-plt.figure(figsize=(12, 6))
-plt.plot(X, Y, "kx", mew=2)
-plt.plot(xx, mean, "C0", lw=2)
-plt.fill_between(
-    xx[:, 0],
-    mean[:, 0] - 1.96 * np.sqrt(var[:, 0]),
-    mean[:, 0] + 1.96 * np.sqrt(var[:, 0]),
-    color="C0",
-    alpha=0.2,
-)
-
-for _ in range(10):
-    w = tfp.distributions.Normal([[0]] * num_rff, [[1]] * num_rff).sample()
-    xx = np.linspace(-0.1, 1.1, 100).reshape(100, 1)  # test points must be of shape (N, D)
-    yy = rff_wsa_fsample(xx, tf.cast(w, dtype=tf.float64))
-    plt.plot(xx, yy, color="C1")
-    # `sample_dgp` returns a callable - which we subsequently evaluate
-    # f_sample: Callable[[tf.Tensor], tf.Tensor] = sample_dgp(dgp)
-    # plt.plot(xx, f_sample(xx).numpy(), color="C1")
-
-plt.plot(xx, samples[:, :, 0].numpy().T, "C0", linewidth=0.5)
-_ = plt.xlim(-0.1, 1.1)
-
-# -
-
-# ------------------
-
-# The same notebook from https://secondmind-labs.github.io/GPflux/notebooks/efficient_sampling.html  
-# With a modificatio of data from another notebook
+# ## 2D case
 
 # +
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
+import tensorflow_probability as tfp
+from tensorflow_probability import distributions as tfd
+from trieste.utils.objectives import branin
+from trieste.space import Box
+from util.plotting import plot_gp_2d, plot_function_2d
 
 import gpflow
-import gpflux
 
-from gpflow.config import default_float
-
-from gpflux.layers.basis_functions.random_fourier_features import RandomFourierFeatures
-from gpflux.sampling import KernelWithFeatureDecomposition
-from gpflux.models.deep_gp import sample_dgp
 
 tf.keras.backend.set_floatx("float64")
-
-# +
-data = np.genfromtxt("util/data/regression_1D.csv", delimiter=",")
-X = data[:, 0].reshape(-1, 1)
-Y = data[:, 1].reshape(-1, 1)
-
-# data = np.load("snelson1d.npz")
-# X, Y = data = data["X"], data["Y"]
-
-num_data, input_dim = X.shape
-
-# +
-kernel = gpflow.kernels.Matern52()
-Z = np.linspace(X.min(), X.max(), 10).reshape(-1, 1).astype(np.float64)
-
-inducing_variable = gpflow.inducing_variables.InducingPoints(Z)
-# gpflow.utilities.set_trainable(inducing_variable, False)
-
-num_rff = 10000
-eigenfunctions = RandomFourierFeatures(kernel, num_rff, dtype=default_float())
-eigenvalues = np.ones((num_rff, 1), dtype=default_float())
-kernel_with_features = KernelWithFeatureDecomposition(kernel, eigenfunctions, eigenvalues)
 # -
 
-layer = gpflux.layers.GPLayer(
-    kernel_with_features,
-    inducing_variable,
-    num_data,
-    whiten=False,
-    num_latent_gps=1,
-    mean_function=gpflow.mean_functions.Zero(),
-)
-likelihood_layer = gpflux.layers.LikelihoodLayer(gpflow.likelihoods.Gaussian())  # noqa: E231
-dgp = gpflux.models.DeepGP([layer], likelihood_layer)
-model = dgp.as_training_model()
+Xs_samples = Box([0.0, 0.0], [1.0, 1.0]).sample(23)
+X = Xs_samples
+Y = branin(X)
+k = gpflow.kernels.RBF(lengthscales=[1.0, 1.0])
+m_2d = gpflow.models.GPR(data=(X, Y), kernel=k, mean_function=None)
+opt = gpflow.optimizers.Scipy()
+opt_logs = opt.minimize(m_2d.training_loss, m_2d.trainable_variables, options=dict(maxiter=100))
+
+m_2d
+
+plot_gp_2d(m_2d,[0.0, 0.0], [1.0, 1.0])
+
+# -------------------------
+
+D = 1000
 
 # +
-model.compile(tf.optimizers.Adam(learning_rate=0.1))
+scaler_l = np.diag(1/np.atleast_1d(m_2d.kernel.lengthscales.numpy())) # [feature num, input dim]
 
-callbacks = [
-    tf.keras.callbacks.ReduceLROnPlateau(
-        monitor="loss",
-        patience=5,
-        factor=0.95,
-        verbose=0,
-        min_lr=1e-6,
-    )
-]
+omega = np.random.randn(D, X.shape[1]) # [feature num, input dim]
 
-history = model.fit(
-    {"inputs": X, "targets": Y},
-    batch_size=num_data,
-    epochs=300,
-    callbacks=callbacks,
-    verbose=0,
-)
+b = 2 * np.pi * np.random.uniform(0, 1, D) # b \sim U[0, 2pi]
+
+b_for_nObservations = np.array([b, ] * X.shape[0]).T  # Nx * b
+
+sigma2 = m_2d.kernel.variance.numpy()
+
+# z(x) = \sqrt(2a/m) cos(Wx + b)
+vector_Z_T = np.sqrt(2 * sigma2 / D) * np.cos(np.matmul(omega, np.matmul(scaler_l, tf.transpose(X))) + b_for_nObservations)
 
 # +
-from typing import Callable
+sigma_le_2 = m_2d.likelihood.variance.numpy()
+# A: φ(x)^T φ(x)  + σ_{le}^2I
+A = np.dot(vector_Z_T, vector_Z_T.T) + sigma_le_2 * np.eye(D)
 
-x_margin = 5
-n_x = 1000
-X_test = np.linspace(X.min() - x_margin, X.max() + x_margin, n_x).reshape(-1, 1)
+# posterior of θ sample: θ|Dn ∼ N(A−1ΦTyn, σ2A−1), shape: (m_ftrs), shape checked correct
+A_inverse = tf.linalg.inv(A).numpy()
 
-f_mean, f_var = dgp.predict_f(X_test)
-f_scale = np.sqrt(f_var)
+# θ(A^{-1} φ(x)^T Y )
+mean_of_post_theta = np.dot(np.dot(A_inverse, vector_Z_T), Y)
+mean_of_post_theta = np.squeeze(np.asarray(mean_of_post_theta))
+variance_of_post_theta = sigma_le_2 * A_inverse
 
-# Plot samples
-n_sim = 10
-for _ in range(n_sim):
-    # `sample_dgp` returns a callable - which we subsequently evaluate
-    f_sample: Callable[[tf.Tensor], tf.Tensor] = sample_dgp(dgp)
-    plt.plot(X_test, f_sample(X_test).numpy())
+theta_sample = np.random.multivariate_normal(mean_of_post_theta, variance_of_post_theta)
 
-# Plot GP mean and uncertainty intervals and data
-plt.plot(X_test, f_mean, "C0")
-plt.plot(X_test, f_mean + f_scale, "C0--")
-plt.plot(X_test, f_mean - f_scale, "C0--")
-plt.plot(X, Y, "kx", alpha=0.2)
-plt.xlim(X.min() - x_margin, X.max() + x_margin)
-plt.ylim(Y.min() - x_margin, Y.max() + x_margin)
-plt.show()
+
+def makeFunc(kernel_var, m_ftrs, W, b, theta):
+    """
+    :param kernel_var kernel variance
+    :param m_ftrs RFF features, a.k.a value of D
+    :param W spectral density sample
+    :param b uniform sample from [0, 2pi]
+    
+    return a sampled approximated posterior trajectory of GP
+    """
+    return lambda x: np.dot(np.sqrt(2 * kernel_var / D) * np.cos(np.dot(omega, np.matmul(scaler_l, np.atleast_2d(x).T)) 
+                                                            + np.atleast_2d(b).T).T, theta)
+
+
+# -
+
+for _ in range(3):
+    theta_sample = np.random.multivariate_normal(mean_of_post_theta, variance_of_post_theta)
+    f_sample = makeFunc(sigma2, D, omega, b, theta_sample)
+    plot_function_2d(f_sample, [0.0, 0.0], [1.0, 1.0])
